3a93a8e95d095da9c7c17ab311c92e3cf3962b08
Cotamor <cotamor@gmail.com>
2022-07-07 16:13:44 +0900

    step-7(Create, update, delete a ticket)

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 
 const User = require("../models/userModel");
 // @desc   Register a new user
-// @route  /api/users
+// @route  POST /api/users
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
@@ -48,7 +48,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
 });
 // @desc   Login user
-// @route  /api/users/login
+// @route  POST /api/users/login
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
@@ -70,7 +70,7 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 // @desc   Get current user
-// @route  /api/users/me
+// @route  GET /api/users/me
 // @access Private
 const getMe = asyncHandler(async (req, res) => {
   const user = {
