ba9c98a98df438f77e93fbec329710a735f78c00
Cotamor <cotamor@gmail.com>
2022-07-09 02:56:16 +0900

    step-8(Tickets functionality)

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../features/auth/authSlice";
+// import { configureStore } from "@reduxjs/toolkit";
+// import authReducer from "../features/auth/authSlice";
+// import ticketReducer from "../features/tickets/ticketSlice";
+
+// export const store = configureStore({
+//   reducer: {
+//     auth: authReducer,
+//     tickets: ticketReducer,
+//   },
+// });
+import { configureStore } from '@reduxjs/toolkit'
+import authReducer from '../features/auth/authSlice'
+import ticketReducer from '../features/tickets/ticketSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    tickets: ticketReducer,
   },
-});
+})
+
